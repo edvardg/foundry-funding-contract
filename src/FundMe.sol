e0db8ec32ef5402cec699b019ebb120f5e25f475
Edvard Gzraryan <edvardgzraryan@gmail.com>
2024-05-31 15:01:38 +0400

    Updated remapping and imports.

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from
-    "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 import {PriceConverter} from "./PriceConverter.sol";
 
 error FundMe__NotOwner();
